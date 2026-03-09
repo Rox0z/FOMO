c390a4416f7b98822f8d2e1ebc768bc42bd7ed3a
DinisAlmeida-cpu <a79895@ualg.pt>
2026-03-09 15:09:15 +0000

    Login/Register for users & vendors

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginVendors } from './login-vendors/login-vendors';
+import { LoginUsers } from './login-users/login-users';
+import { RegisterVendors } from './register-vendors/register-vendors';
+import { RegisterUsers } from './register-users/register-users';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login-vendors',
+    component: LoginVendors
+  },
+  {
+    path: 'register-vendors',
+    component: RegisterVendors
+  },
+  {
+    path: 'login-users',
+    component: LoginUsers
+  },
+  {
+    path: 'register-users',
+    component: RegisterUsers
+  }
+];
